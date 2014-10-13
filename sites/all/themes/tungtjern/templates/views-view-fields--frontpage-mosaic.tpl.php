21d3d219f254e593f7ecd8d0287398ca5a17bcf6
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-13 13:56:59 +0200

    Updated frontpage mosaic views template file to not be dependent on draggableviews module.

--- sites/all/themes/tungtjern/templates/views-view-fields--frontpage-mosaic.tpl.php
+++ sites/all/themes/tungtjern/templates/views-view-fields--frontpage-mosaic.tpl.php
@@ -1,8 +1,8 @@
 <?php $node = node_load($row->nid); ?>
-<?php if (is_numeric($row->draggableviews_structure_weight) && $row->draggableviews_structure_weight == 0) : ?>
+<?php if ($view->row_index == 0) : ?>
   <?php $node_view = node_view($node, 'topfront'); ?>
   <?php print render($node_view); ?>
-<?php elseif (is_numeric($row->draggableviews_structure_weight) && $row->draggableviews_structure_weight < 4) : ?>
+<?php elseif ($view->row_index < 4) : ?>
   <?php $node_view = node_view($node, 'focus'); ?>
   <?php print render($node_view); ?>
 <?php else: ?>
