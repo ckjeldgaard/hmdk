2f127b5315b1ad5e54685cea42a2eefed7af5b14
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-13 13:17:39 +0200

    Fixed PHP strict warning in frontpage mosaic view and updated metatag module to version 7.x-1.4.

--- sites/all/themes/tungtjern/templates/views-view-fields--frontpage-mosaic.tpl.php
+++ sites/all/themes/tungtjern/templates/views-view-fields--frontpage-mosaic.tpl.php
@@ -1,7 +1,11 @@
+<?php $node = node_load($row->nid); ?>
 <?php if (is_numeric($row->draggableviews_structure_weight) && $row->draggableviews_structure_weight == 0) : ?>
-  <?php print render(node_view(node_load($row->nid), 'topfront')); ?>
+  <?php $node_view = node_view($node, 'topfront'); ?>
+  <?php print render($node_view); ?>
 <?php elseif (is_numeric($row->draggableviews_structure_weight) && $row->draggableviews_structure_weight < 4) : ?>
-  <?php print render(node_view(node_load($row->nid), 'focus')); ?>
+  <?php $node_view = node_view($node, 'focus'); ?>
+  <?php print render($node_view); ?>
 <?php else: ?>
-  <?php print render(node_view(node_load($row->nid), 'teaser')); ?>
+  <?php $node_view = node_view($node, 'teaser'); ?>
+  <?php print render($node_view); ?>
 <?php endif;?>
