ad2b33612070c126135fd6ffb6627499727f1a06
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-14 08:29:02 +0200

    Fixed PHP strict warning bug in related artist block template.

--- sites/all/themes/tungtjern/templates/block--related-artist.tpl.php
+++ sites/all/themes/tungtjern/templates/block--related-artist.tpl.php
@@ -2,6 +2,6 @@
   <div class="related-artist">
     <h3><?php print t('Read also'); ?></h3>
     <p class="related-artist-description"><?php print t("If you're interested in %artist, you might also like %related_artist. Check out our review of %review:", array('%artist' => $content['artist'], '%related_artist' => $content['related_artist'], '%review' => $content['review_node']->title)); ?></p>
-    <?php print render(node_view($content['review_node'], 'teaser')); ?>
+    <?php $node_view = node_view($content['review_node'], 'teaser'); print render($node_view); ?>
   </div>
 <?php endif; ?>
