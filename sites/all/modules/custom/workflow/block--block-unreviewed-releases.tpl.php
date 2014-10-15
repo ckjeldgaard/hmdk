505d6dcc7d376782e06c6f8de7d350a53a3f0c7c
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-15 10:44:56 +0200

    Added button to delete all unreviewed releases in workflow module.

--- sites/all/modules/custom/workflow/block--block-unreviewed-releases.tpl.php
+++ sites/all/modules/custom/workflow/block--block-unreviewed-releases.tpl.php
@@ -1,2 +1,5 @@
 <p>Udgivelser som ikke har nogen tilknyttet anmeldelse (<?php print $content['count']; ?> stk. i alt).</p>
 <?php print $content['table']; ?>
+<div class="delete-all"><br />
+  <?php print render($content['form']); ?>
+</div>
