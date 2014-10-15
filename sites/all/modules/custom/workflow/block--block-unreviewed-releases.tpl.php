d60e07dc712ec8fe19a168a63a032ec325d5f07a
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-15 10:54:32 +0200

    Added custom permission to workflow module to check for permission to delete all unreviewed releases.

--- sites/all/modules/custom/workflow/block--block-unreviewed-releases.tpl.php
+++ sites/all/modules/custom/workflow/block--block-unreviewed-releases.tpl.php
@@ -1,5 +1,7 @@
 <p>Udgivelser som ikke har nogen tilknyttet anmeldelse (<?php print $content['count']; ?> stk. i alt).</p>
 <?php print $content['table']; ?>
+<?php if (user_access('delete unreviewed releases')) : ?>
 <div class="delete-all"><br />
   <?php print render($content['form']); ?>
 </div>
+<?php endif; ?>
