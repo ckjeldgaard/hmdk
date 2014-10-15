aa2690b04d1e3633cb67b3c7a4070fd5ff2490db
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-15 11:03:03 +0200

    Added number of unreviewed releases to js alert in workflow module.

--- sites/all/modules/custom/workflow/block--block-unreviewed-releases.tpl.php
+++ sites/all/modules/custom/workflow/block--block-unreviewed-releases.tpl.php
@@ -1,6 +1,6 @@
 <p>Udgivelser som ikke har nogen tilknyttet anmeldelse (<?php print $content['count']; ?> stk. i alt).</p>
 <?php print $content['table']; ?>
-<?php if (user_access('delete unreviewed releases')) : ?>
+<?php if (user_access('delete unreviewed releases') && $content['count'] > 0) : ?>
 <div class="delete-all"><br />
   <?php print render($content['form']); ?>
 </div>
