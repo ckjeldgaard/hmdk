a259115902949bec8dcc4d3c29e0cec680a50b89
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-20 16:05:40 +0200

    Fixed RSS cdata bug.

--- sites/all/modules/custom/rss/rss.tpl.php
+++ sites/all/modules/custom/rss/rss.tpl.php
@@ -7,9 +7,9 @@
   <language>da</language>
 <?php foreach ($rss_items as $node) : ?>
   <item>
-    <title><?php print $node->rss_title; ?></title>
+    <title><![CDATA[<?php print $node->rss_title; ?>]]></title>
     <link><?php print url('node/' . $node->nid, array('absolute' => TRUE)); ?>?utm_source=RSS_feed&amp;utm_medium=RSS&amp;utm_campaign=RSS_Syndication</link>
-    <description><?php print $node->desc; ?></description>
+    <description><![CDATA[<?php print $node->desc; ?>]]></description>
     <pubDate><?php print date("D, d M Y H:i:s", $node->published_at); ?> +0200</pubDate>
     <dc:creator><?php print $node->name; ?></dc:creator>
   </item>
