b68611f5882d440e0d4f2e422e64b5f1efc64887
Christoffer Kjeldgaard Petersen <ckp@trifork.com>
2014-10-21 09:12:16 +0200

    Fixed address block name.

--- sites/all/modules/custom/address_block/block--address-block.tpl.php
+++ sites/all/modules/custom/address_block/block--address-block.tpl.php
@@ -6,7 +6,7 @@
   <address itemscope itemtype="http://schema.org/Organization">
     <strong itemprop="name"><?php print $content->organization_name; ?></strong><br />
     <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-      <span itemprop="name"><?php print $content->name; ?></span><br />
+      <?php if (strlen($content->name) > 0) : ?><span itemprop="name"><?php print $content->name; ?></span><br /><?php endif; ?>
       <span itemprop="streetAddress"><?php print $content->street_address; ?></span><br />
       <span itemprop="postalCode"><?php print $content->postal_code; ?></span> <span itemprop="addressLocality"><?php print $content->address_locality; ?></span><br />
       <span itemprop="addressCountry"><?php print $content->country; ?></span><br />
