da37b4fc80c0526149d0ff5f5a2c15dfc140f124
doctortoffer <ckp@trifork.com>
2015-03-11 10:51:03 +0100

    Removed schema.org structured data from latest concerts sidebar block.

--- sites/all/modules/custom/sidebar_blocks/block--latest-concerts.tpl.php
+++ sites/all/modules/custom/sidebar_blocks/block--latest-concerts.tpl.php
@@ -4,16 +4,16 @@
   <div id="next-concerts">
     <ul class="bjqs next-concerts-slide">
     <?php foreach ($concerts as $c) : ?>
-      <li itemprop="event" itemscope itemtype="http://schema.org/Event">
-	<h3 class="concert-name" itemprop="name"><?php print $c->artist_string; ?></h3>
+      <li>
+	<h3 class="concert-name"><?php print $c->artist_string; ?></h3>
 	<p class="concert-date">
-	  <time datetime="<?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value'], 'date'); ?>" itemprop="startDate" content="<?php print date ('Y-m-d', $c->field_event_date[LANGUAGE_NONE][0]['value']); ?>"><?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value'], 'displaydate'); ?></time>
+	  <time datetime="<?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value'], 'date'); ?>" content="<?php print date ('Y-m-d', $c->field_event_date[LANGUAGE_NONE][0]['value']); ?>"><?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value'], 'displaydate'); ?></time>
 	  <?php if ($c->field_event_date[LANGUAGE_NONE][0]['value'] != $c->field_event_date[LANGUAGE_NONE][0]['value2']) : ?>
-	  - <time datetime="<?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value2'], 'date'); ?>" itemprop="endDate" content="<?php print date ('Y-m-d', $c->field_event_date[LANGUAGE_NONE][0]['value2']); ?>"><?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value2'], 'displaydate'); ?></time>
+	  - <time datetime="<?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value2'], 'date'); ?>" content="<?php print date ('Y-m-d', $c->field_event_date[LANGUAGE_NONE][0]['value2']); ?>"><?php print format_date($c->field_event_date[LANGUAGE_NONE][0]['value2'], 'displaydate'); ?></time>
 	  <?php endif; ?>
 	</p>
-	<p class="concert-location" itemprop="location" itemscope itemtype="http://schema.org/Place">
-	  <a href="<?php print url('taxonomy/term/' . $c->field_venue[LANGUAGE_NONE][0]['tid']); ?>" itemprop="url"><?php print _get_venue_name($c->field_venue[LANGUAGE_NONE][0]['tid']); ?></a>
+	<p class="concert-location">
+	  <a href="<?php print url('taxonomy/term/' . $c->field_venue[LANGUAGE_NONE][0]['tid']); ?>"><?php print _get_venue_name($c->field_venue[LANGUAGE_NONE][0]['tid']); ?></a>
 	</p>
       </li>
     <?php endforeach; ?>
