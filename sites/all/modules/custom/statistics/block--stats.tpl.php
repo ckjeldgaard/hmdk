caff0f554402a65d8489dac3349b6cf6b8d0c1ec
Christoffer Kjeldgaard Petersen <christoffer@sappy.dk>
2017-01-11 10:17:58 +0100

    Added last year graph to statistics page.

--- sites/all/modules/custom/statistics/block--stats.tpl.php
+++ sites/all/modules/custom/statistics/block--stats.tpl.php
@@ -72,6 +72,39 @@
 <div id="ratings_overall" style="width: 750px; height: 450px;"></div>
 <?php endif; ?>
 
+<?php if ($ratings_last_year) : ?>
+  <p>Sidste år (<?php print (date('Y') - 1); ?>):</p>
+  <script type="text/javascript">
+    google.load("visualization", "1", {packages:["corechart"]});
+    google.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Karakter", "Antal anmeldelser"],
+        <?php foreach ($ratings_last_year as $r => $q): ?>
+        ["<?php print $r; ?>", <?php print $q; ?>],
+        <?php endforeach; ?>
+      ]);
+
+      var options = {
+        title: "Sidste år (<?php print (date('Y') - 1); ?>)",
+        width: 750,
+        height: 450,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+        hAxis: {
+          title: 'Karakterer',
+        },
+        vAxis: {
+          title: 'Antal anmeldelser'
+        }
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("ratings_last_year"));
+      chart.draw(data, options);
+  }
+  </script>
+<div id="ratings_last_year" style="width: 750px; height: 450px;"></div>
+<?php endif; ?>
+
 <?php if ($ratings_year) : ?>
   <p>År til dato:</p>
   <script type="text/javascript">
